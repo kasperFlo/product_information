6a16c70cff93328ec93acd1bc6559f6d0f519d14
Florian <20kasperbauefr2@gmail.com>
2022-11-01 18:01:19 -0400

    finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import traceback
+from random import randint
 class product:
     def __init__(self,pC=111,pN="default",pSP=1,pMC=1,sL=1,eMUM=1) -> None:
         self.productCode = pC #(e.g., an integer from 100 to 1000)
@@ -57,6 +57,13 @@ class product:
         if (isinstance(args,int)) and args >= 0:
             self._estimatedMonthlyUnitsManufactured = args
         else: raise TypeError("an integer greater than or equal to 0")
+    def next12Months(self):
+        print(f"{'*'*30}\nProduct Code: {self.productCode}\nProduct Name: {self.productName}\n\n Sale Price :{self.productSalePrice}CAD\nManifacture Cost : {self.productManufactureCost}\nMonthly Production : {self.estimatedMonthlyUnitsManufactured} Units\n\n{'*'*30}")
+        stock = self.stockLevel
+        for i in range(1,13):
+            diviation = randint(-10,11)
+            stock -= diviation
+            print(f"Month {i}:\n{' '*5}Manufactured: {self.estimatedMonthlyUnitsManufactured} Units\n{' '*5}Sold: {stock+diviation}\n{' '*5}Stock: {(stock)}")
 
 coc = product()
 while True:
@@ -89,3 +96,4 @@ while True:
         coc.estimatedMonthlyUnitsManufactured = int(input("Enter Estimated Monthly Units Manufactured : ")) #(e.g. an integer greater than or equal to 0)
         break
     except : pass 
+coc.next12Months()
