2b9e1e3689d6346f8830272347238b2d084a3b56
Florian <20kasperbauefr2@gmail.com>
2022-11-03 21:32:44 -0400

    added printing errors

--- operator.py
+++ operator.py
@@ -10,30 +10,30 @@ while True:
     try:
         coc.productCode = int(input("Enter Product Code : ")) #(e.g. an integer greater than or equal to 0)
         break 
-    except : pass
+    except Exception as e: print(e)
 while True:
     try:
         coc.productName = input("Enter Product Name : ") # must be a String
         break
-    except : pass
+    except Exception as e: print(e)
 while True:
     try:
         coc.productSalePrice = float(input("Enter Sales Price : "))#(e.g., a real number greater than zero)
         break
-    except : pass 
+    except Exception as e: print(e) 
 while True:
     try:
         coc.productManufactureCost = float(input("Enter product Manufacture Cost : ")) #(e.g., a real number greater than zero)
         break
-    except : pass
+    except Exception as e: print(e)
 while True:
     try:
         coc.stockLevel = input("Enter stockLevel : ") #(an integer number greater than 0)
         break
-    except : pass
+    except Exception as e: print(e)
 while True:
     try:
         coc.estimatedMonthlyUnitsManufactured = int(input("Enter Estimated Monthly Units Manufactured : ")) #(e.g. an integer greater than or equal to 0)
         break
-    except : pass 
+    except Exception as e: print(e) 
 coc.next12Months()
