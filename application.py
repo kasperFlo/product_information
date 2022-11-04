5087ea3aabf418b7a51d70e3b0846f3854fd820c
Florian <20kasperbauefr2@gmail.com>
2022-11-04 14:50:48 -0400

    finalized variable names

--- application.py
+++ application.py
@@ -1,35 +1,37 @@
 import main
-coc = main.product()
+productNum1 = main.product()
 while True:
     try:
-        coc.productCode = int(input("Enter Product Code : ")) #(e.g. an integer greater than or equal to 0)
+        productNum1.productCode = int(input("Enter Product Code : ")) #(e.g. an integer greater than or equal to 0)
         break 
     except : print("an integer from 100 to 1000")
 while True:
     try:
-        coc.productName = input("Enter Product Name : ") # must be a String
+        productNum1.productName = input("Enter Product Name : ") # must be a String
         break
     except : print("only a string ")
 while True:
     try:
-        coc.productSalePrice = float(input("Enter Sales Price : "))#(e.g., a real number greater than zero)
+        productNum1.productSalePrice = float(input("Enter Sales Price : "))#(e.g., a real number greater than zero)
         break
     except : print("a real number greater than zero ")
 while True:
     try:
-        coc.productManufactureCost = float(input("Enter product Manufacture Cost : ")) #(e.g., a real number greater than zero)
+        productNum1.productManufactureCost = float(input("Enter product Manufacture Cost : ")) #(e.g., a real number greater than zero)
         break
     except : print("a real number greater than zero ")
 while True:
     try:
-        coc.stockLevel = input("Enter stockLevel : ") #(an integer number greater than 0)
+        productNum1.stockLevel = input("Enter stockLevel : ") #(an integer number greater than 0)
         break
     except : print("an integer number greater than 0")
 while True:
     try:
-        coc.estimatedMonthlyUnitsManufactured = int(input("Enter Estimated Monthly Units Manufactured : ")) #(e.g. an integer greater than or equal to 0)
+        productNum1.estimatedMonthlyUnitsManufactured = int(input("Enter Estimated Monthly Units Manufactured : ")) #(e.g. an integer greater than or equal to 0)
         break
     except : print("an integer greater than or equal to 0")
+
+productNum1.next12Months()
 
 # coc.productCode = 642
 # coc.productName = "cocandbalz"
@@ -37,4 +39,3 @@ while True:
 # coc.productManufactureCost = 1
 # coc.stockLevel = 100
 # coc.estimatedMonthlyUnitsManufactured = 120
-coc.next12Months()
