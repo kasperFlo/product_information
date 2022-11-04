c50b3d2f00e2abc9966d191b35d03ee944ce4e9e
Florian <20kasperbauefr2@gmail.com>
2022-11-04 13:32:11 -0400

    changed from test inputs back to input loops

---
+++ application.py
@@ -0,0 +1,40 @@
+import main
+coc = main.product()
+while True:
+    try:
+        coc.productCode = int(input("Enter Product Code : ")) #(e.g. an integer greater than or equal to 0)
+        break 
+    except : print("an integer from 100 to 1000")
+while True:
+    try:
+        coc.productName = input("Enter Product Name : ") # must be a String
+        break
+    except : print("only a string ")
+while True:
+    try:
+        coc.productSalePrice = float(input("Enter Sales Price : "))#(e.g., a real number greater than zero)
+        break
+    except : print("a real number greater than zero ")
+while True:
+    try:
+        coc.productManufactureCost = float(input("Enter product Manufacture Cost : ")) #(e.g., a real number greater than zero)
+        break
+    except : print("a real number greater than zero ")
+while True:
+    try:
+        coc.stockLevel = input("Enter stockLevel : ") #(an integer number greater than 0)
+        break
+    except : print("an integer number greater than 0")
+while True:
+    try:
+        coc.estimatedMonthlyUnitsManufactured = int(input("Enter Estimated Monthly Units Manufactured : ")) #(e.g. an integer greater than or equal to 0)
+        break
+    except : print("an integer greater than or equal to 0")
+
+# coc.productCode = 642
+# coc.productName = "cocandbalz"
+# coc.productSalePrice = 3
+# coc.productManufactureCost = 1
+# coc.stockLevel = 100
+# coc.estimatedMonthlyUnitsManufactured = 120
+coc.next12Months()
