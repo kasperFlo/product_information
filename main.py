d73a34abad847cb6c5008f1074667db3b206f01e
Florian <20kasperbauefr2@gmail.com>
2022-11-05 02:12:29 -0400

    slghtly addded comments

--- main.py
+++ main.py
@@ -11,49 +11,49 @@ class product:
     def productCode(self):
         return self._productCode
     @productCode.setter
-    def productCode(self,args:int):
+    def productCode(self,args:int): # check arguments
         if ((str(args)).isnumeric() and args >= 100 and args <= 1000): self._productCode = args  
         else : raise TypeError
     @property
     def productName(self):
         return self._productName
     @productName.setter
-    def productName(self,args):
+    def productName(self,args): # check arguments
         if (args.isalpha()): self._productName = args
         else : raise TypeError
     @property
     def productSalePrice(self):
         return self._productSalePrice
     @productSalePrice.setter    
-    def productSalePrice(self,args:float):
+    def productSalePrice(self,args:float): # check arguments
         if (isinstance(args,(int,float)) and args > 0): self._productSalePrice = args 
         else : raise TypeError
     @property
     def productManufactureCost(self):
         return self._productManufactureCost
     @productManufactureCost.setter
-    def productManufactureCost(self,args):
+    def productManufactureCost(self,args): # check arguments
          if (isinstance(args,(int,float)) and args > 0): self._productManufactureCost = args
          else : raise TypeError
     @property
     def stockLevel(self):
         return self._stockLevel
     @stockLevel.setter
-    def stockLevel(self,args):
+    def stockLevel(self,args):  # check arguments
         if (str(args) != str(float(args))) and int(args) > 0: self._stockLevel = int(args) 
         else : raise TypeError
     @property
     def estimatedMonthlyUnitsManufactured(self):
         return self._estimatedMonthlyUnitsManufactured
-    @estimatedMonthlyUnitsManufactured.setter
-    def estimatedMonthlyUnitsManufactured(self,args):
+    @estimatedMonthlyUnitsManufactured.setter 
+    def estimatedMonthlyUnitsManufactured(self,args):  # check arguments
         if (isinstance(args,int)) and args >= 0: self._estimatedMonthlyUnitsManufactured = args 
         else :raise TypeError
     def next12Months(self):
         print(f"{'*'*30}\nProduct Code: {self.productCode}\nProduct Name: {self.productName}\n\nSale Price : {self.productSalePrice} CAD\nManifacture Cost : {self.productManufactureCost}\nMonthly Production : {self.estimatedMonthlyUnitsManufactured} Units\n\n{'*'*30}")
         stock = self.stockLevel
         totalSold = 0
-        for i in range(1,13):
+        for i in range(1,13): #check for within months 1 - 12 then netprofits and netsold
             diviation = randint(-10,11)
             stock -= diviation
             print(f"Month {i}:\n*{' '*5}Manufactured: {self.estimatedMonthlyUnitsManufactured} Units\n*{' '*5}Sold: {stock+diviation}\n*{' '*5}Stock: {(stock)}")
